14cc03ebadc2fe5ac201f289ee9f5b4c810af523
lineberner <line437x@stud.kea.dk>
2022-06-21 13:19:09 +0200

    fix next artist

--- src/Components/Playing_now/Current_artist/Current.js
+++ src/Components/Playing_now/Current_artist/Current.js
@@ -1,9 +1,10 @@
 import Act from "../../Schedule/Act";
+import { v4 as uuidv4 } from "uuid";
 
 export default function CurrentArtist(props) {
 	return (
 		<div id="current_artist">
-			<Act {...props}></Act>
+			<Act {...props} key={uuidv4()}></Act>
 		</div>
 	);
 }
